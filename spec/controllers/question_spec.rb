574adc22c99b7ce7582a4fab3b6d59a68f1d6774
Anna Jeszeck & John Simoni <pair+ajeszeck.jsimoni42@devbootcamp.com>
2017-09-28 15:51:40 -0700

    Refactored tests

--- spec/controllers/question_spec.rb
+++ spec/controllers/question_spec.rb
@@ -1,15 +1,19 @@
 require 'spec_helper'
 
 describe "Questions Controller" do
-  it "it shows an erb GET /questions" do
-    get '/questions'
-    expect(last_response.status).to eq(200)
+  describe "get /questions" do
+    it "returns 200" do
+      get '/questions'
+      expect(last_response.status).to eq(200)
+    end
   end
-  it "it reroutes to landing page POST /questions" do
-    post '/questions', {question_text: "Nothing"}
-    expect(last_response.location).to end_with("/questions")
-  end
-  it "it creates a new question on form submit" do
-    expect{post '/questions', {question_text: "Nothing"}}.to change{Question.count}.by(1)
+  describe "post /questions" do
+    it "it reroutes to landing page" do
+      post '/questions', {question_text: "Nothing"}
+      expect(last_response.location).to end_with("/questions")
+    end
+    it "it creates a new question on form submit" do
+      expect{post '/questions', {question_text: "Nothing"}}.to change{Question.count}.by(1)
+    end
   end
 end
