533a30a6436d2f4e882470ba8ac2f6b8aa2c19cc
Anna Jeszeck & John Simoni <pair+ajeszeck.jsimoni42@devbootcamp.com>
2017-09-28 15:46:28 -0700

    add code to allow users to post questions

--- spec/controllers/question_spec.rb
+++ spec/controllers/question_spec.rb
@@ -5,4 +5,11 @@ describe "Questions Controller" do
     get '/questions'
     expect(last_response.status).to eq(200)
   end
+  it "it reroutes to landing page POST /questions" do
+    post '/questions', {question_text: "Nothing"}
+    expect(last_response.location).to end_with("/questions")
+  end
+  it "it creates a new question on form submit" do
+    expect{post '/questions', {question_text: "Nothing"}}.to change{Question.count}.by(1)
+  end
 end
