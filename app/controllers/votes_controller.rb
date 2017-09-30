5f27a81b821daaf7bbbbf3adc7a213a935651daa
John Simoni <jsimoni03@gmail.com>
2017-09-30 07:38:29 -0700

    Refactored votes controller, configed db to work on hans' laptop

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,14 @@
 post "/questions/:voteable_id/votes" do
   type = params[:type]
+  voteable = nil
   case type
   when "question"
-    question = Question.find_by_id(params[:voteable_id])
-    question.votes << Vote.new(vote_val: params[:vote_val], user_id: 0)
+    voteable = Question.find_by_id(params[:voteable_id])
   when "answer"
     # FIXME: Change user_id when we add sessions!!!!
-    answer = Answer.find_by_id(params[:voteable_id])
-    answer << Vote.new(vote_val: params[:vote_val], user_id: 0)
+    voteable = Answer.find_by_id(params[:voteable_id])
   else
     status 404
   end
+  voteable.votes << Vote.new(value: params[:vote_val], user_id: 0) 
 end
