cfe65aa1f652206bdff71ecaf4138e7b67904035
Chris Buell & John Simoni <pair+ChristineBuell.jsimoni42@devbootcamp.com>
2017-09-29 17:04:04 -0700

    Wrote tests for vote controller

--- spec/controllers/vote_spec.rb
+++ spec/controllers/vote_spec.rb
@@ -5,18 +5,35 @@ describe "Vote Controller" do
   let (:question) {Question.create(question_text: "Where?", user_id: user.id)}
   describe "post ../votes" do
     context "upvote on a question" do
+      let(:upvote) {post "/questions/#{question.id}/votes", {vote_val: 1}}
       it "returns 302" do
+        upvote
         expect(last_response.status).to eq(302)
       end
       it "redirects to question show page" do
-        expect(last_response.location).to end_with()
+        upvote
+        expect(last_response.location).to end_with("#{question.id}")
+      end
+      it "increments vote count on question by one" do
+        expect{upvote}.to change{question.votes.count}.by(1)
       end
     end
     context "downvote on a question" do
-
+      let(:downvote) {post "/questions/#{question.id}/votes", {vote_val: -1}}
+      it "returns 302" do
+        downvote
+        expect(last_response.status).to eq(302)
+      end
+      it "redirects to question show page" do
+        downvote
+        expect(last_response.location).to end_with("#{question.id}")
+      end
+      it "decrements vote count on question by one" do
+        expect{downvote}.to change{question.votes.count}.by(-1)
+      end
     end
     context "user is not logged in" do
-
+      it "returns 404"
     end
   end
 end
