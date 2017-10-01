f01657ddde3551c87bb666e5f72e7a922517faf8
Piper Denney <denney.piper@gmail.com>
2017-10-01 16:25:43 -0700

    Add sign up form/erb and create password methods in user model.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+get '/users/new' do
+  erb :'users/new'
+end
+
+post '/users' do
+  user = User.new(params[:user])
+  user.password = params[:user][:password_hash]
+  if user.save
+    @message = "Welcome to the Family, Troll!"
+    
+    redirect '/questions'
+  else
+    status 422
+    @errors = user.errors.full_messages
+    redirect '/users/new'
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
   has_many :answers
   has_many :questions
@@ -5,4 +7,21 @@ class User < ActiveRecord::Base
   validates :user_name, presence: true
   validates :email, presence: true
   validates :password_hash, presence: true
+
+  include BCrypt
+
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)
+    return user if user && user.password == password
+    nil
+  end
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
