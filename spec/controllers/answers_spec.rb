a000461771b3334d188c6876d33ef19d150840fc
John Simoni <email": null>
2017-10-01 20:44:32 -0700

    post comment for answer returns 404 when user is not logged in

--- spec/controllers/answers_spec.rb
+++ spec/controllers/answers_spec.rb
@@ -26,4 +26,27 @@ describe "answers_controller" do
       expect(Answer.find_by_answer_text("here")).to_not be_nil
     end
   end
+
+  describe "new answer comment" do
+    let (:user) {User.create!(user_name: "name", email: "hi@hi.com", password_hash: 'password')}
+    let (:question) {Question.create!(question_text: "question", user_id: user.id)}
+    let (:answer) {Answer.create!(answer_text: "answer", user_id: user.id, question_id: question.id)}
+    let(:post_comment) {post "/questions/#{question.id}/answers/#{answer.id}/comments", {answer_id: answer.id, comment_text: "Hi"}, "rack.session" => {user_id: user.id}}
+    context "when user is logged in" do
+      it "returns 302" do
+        post_comment
+        expect(last_response.status).to eq(302)
+      end
+      it "creates a new comment on answer" do
+        post_comment
+        expect(Answer.find(answer.id).comments.pluck(:comment_text)).to include("Hi")
+      end
+    end
+    context "when user is not logged in" do
+      it "returns 404" do
+        post "/questions/#{question.id}/answers/#{answer.id}/comments", {answer_id: answer.id, comment_text: "Hi"}
+        expect(last_response.status).to eq(404)
+      end
+    end
+  end
 end
