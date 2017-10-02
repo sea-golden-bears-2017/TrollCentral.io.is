6501fd41bc1629c76839c1c905582b9831a2c2f6
Piper Denney <denney.piper@gmail.com>
2017-10-01 21:53:43 -0700

    Add styling to show page.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,14 +4,16 @@ post "/questions/:voteable_id/votes" do
   case type
   when "question"
     voteable = Question.find_by_id(params[:voteable_id])
+    question_id = voteable.id
   when "answer"
     # FIXME: Change user_id when we add sessions!!!!
     voteable = Answer.find_by_id(params[:voteable_id])
+    question_id = voteable.question.id
   else
     status 404
     return "Error - 404"
   end
   user = User.find_by_id(params[:user_id])
   Vote.create!(value: params[:vote_val], user: user, voteable: voteable)
-  redirect "/questions/#{params[:voteable_id]}"
+  redirect "/questions/#{question_id}"
 end
