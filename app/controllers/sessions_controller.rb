aa23c73fde30ef0defaa0430fdfee79867c42fb8
Piper Denney <denney.piper@gmail.com>
2017-10-01 18:42:53 -0700

    Add error handling to login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,27 @@
-post '/sessions' do
+put '/sessions' do
   user = User.authenticate(params[:email], params[:password])
-  if user
+  p user
+  if user != nil
+    p "new session"
     session[:user_id] = user.id
-    redirect '/questions'
-  else
-    status 422
-    @errors = ["User email, password combination is incorrect."]
+    p session
     redirect '/questions'
   end
+    p "user not found"
+    # status 422
+    p session
+    @errors = ["User email, password combination is incorrect."]
+    @questions = Question.all
+    erb :'/questions/index'
 end
 
 delete '/sessions' do
-  if session
-    session.clear
+  if session[:user_id]
+    session.destroy
+    p session
     redirect '/questions'
   else
+    p "not logged in."
     redirect '/questions'
   end
 end
